a541b44c4fc92ae704a329d2903872b276fd423f
unknown <RocKa840@ltpemdomain.local>
2020-02-03 09:50:09 +0100

    jjj

---
+++ EXERCISE TWO/two.js
@@ -0,0 +1,12 @@
+let array3_a = [2, 2, 1, 3];
+let array3_b = [1, 2, 0, 3];
+
+let a = document.getElementById("a");
+let b = document.getElementById("b");
+
+function operation() {
+    let firstNum = array3_a[a.value]; // a == 3
+    let secondNum = array3_b[b.value];
+
+    let
+}
